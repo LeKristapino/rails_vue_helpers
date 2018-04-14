1d657f7e91a6855d70faed578da6c6f19ef65e3e
Kristaps Kulikovskis <kristaps.kulikovskis@makit.lv>
2018-04-15 01:31:07 +0300

    require builder

--- lib/rails_vue_helpers/vue_helper.rb
+++ lib/rails_vue_helpers/vue_helper.rb
@@ -1,3 +1,5 @@
+require 'rails_vue_helpers/vue_component_builder'
+
 module RailsVueHelpers::VueHelper
   def vue_component(component_name, **props, &block)
     RailsVueHelpers::VueComponentBuilder.create(component_name, **props, &block)
