00e57e0d5098137fdb91727d15c8301be88d5fd8
Kristaps Kulikovskis <kristaps.kulikovskis@makit.lv>
2018-04-14 23:30:35 +0300

    Add html builder class

---
+++ lib/rails_vue_helpers/vue_component_builder.rb
@@ -0,0 +1,68 @@
+class RailsVueHelpers::VueComponentCreator
+  def self.create(component_name, **props, &block)
+    self.class.new(component_name, **props).to_html(&block)
+  end
+
+  def initialize(component_name, **props)
+    @component_name = component_name
+    @props = props
+    @raw = @props[:raw]
+  end
+
+  def to_html(&block)
+    "<#{sanitized_component_name} #{attributes} #{@raw}>#{capture(&block) if block_given?}</#{sanitized_component_name}>".html_safe
+  end
+
+  def sanitized_component_name
+    @component_name.to_s.underscore.tr('_', '-')
+  end
+
+  private
+
+  def attributes
+    [binded_props, event_props, regular_props].join(' ')
+  end
+
+  def binded_props
+    binded_props = @props[:binded] || {}
+    binded_props.map { |key, value| transpile_binded_prop(key, value) }.join(' ')
+  end
+
+  def event_props
+    event_props = @props[:events] || {}
+    event_props.map { |key, value| transpile_event_prop(key, value) }.join(' ')
+  end
+
+  def regular_props
+    regular_props = @props.reject { |key, value| [:events, :binded, :raw].include?(key) }
+    regular_props.map { |key, value| transpile_regular_prop(key, value) }.join(' ')
+  end
+
+  def transpile_regular_prop(key, value)
+    normalized_key = key.to_s.underscore.tr('_', '-')
+    normalized_value = value
+
+    if value.is_a?(Array)
+      normalized_value = value.to_s.tr('"', '\'')
+    elsif value =~ /"/
+      normalized_value = value.tr('"', '\'')
+    end
+    "#{normalized_key}=\"#{normalized_value}\" "
+  end
+
+  def transpile_binded_prop(key,value)
+    normalized_key = key.to_s.underscore.tr('_', '-')
+    normalized_value = value
+    unless value.is_a?(String)
+      # if there are some single quotes in the text, make them in ASCII
+      # so HTML doesn't brake
+      normalized_value = value.to_json.gsub("'", "&#39")
+    end
+    "v-bind:#{normalized_key}='#{normalized_value}'"
+  end
+
+  def transpile_event_prop(key, value)
+    normalized_key = key.to_s.underscore.tr('_', '-')
+    "v-on:#{normalized_key}='#{value}'"
+  end
+end
--- lib/rails_vue_helpers/vue_helper.rb
+++ lib/rails_vue_helpers/vue_helper.rb
@@ -1,48 +1,5 @@
 module RailsVueHelpers::VueHelper
   def vue_component(component_name, **props, &block)
-    component_name = component_name.underscore.tr('_', '-')
-    raw = props.delete(:raw)
-    if block_given?
-      "<#{component_name} #{attribute_options(props)} #{raw}>#{capture(&block)}</#{component_name}>".html_safe
-    else
-      "<#{component_name} #{attribute_options(props)} #{raw}></#{component_name}>".html_safe
-    end
-  end
-
-  private
-
-  def attribute_options(attributes = {})
-    binded_props = attributes.delete(:binded) || {}
-    event_props = attributes.delete(:events) || {}
-    result_attrs = ''
-
-    attributes.each do |key, value|
-      normalized_key = key.to_s.underscore.tr('_', '-')
-      normalized_value = value
-
-      if value.is_a?(Array)
-        normalized_value = value.to_s.tr('"', '\'')
-      elsif value =~ /"/
-        normalized_value = value.tr('"', '\'')
-      end
-      result_attrs += "#{normalized_key}=\"#{normalized_value}\" "
-    end
-
-    binded_props.each do |key, value|
-      normalized_key = key.to_s.underscore.tr('_', '-')
-      normalized_value = value
-      unless value.is_a?(String)
-        # if there are some single quotes in the text, make them in ASCII
-        # so HTML doesn't brake
-        normalized_value = value.to_json.gsub("'", "&#39")
-      end
-      result_attrs += "v-bind:#{normalized_key}='#{normalized_value}' "
-    end
-
-    event_props.each do |key, value|
-      normalized_key = key.to_s.underscore.tr('_', '-')
-      result_attrs += "v-on:#{normalized_key}='#{value}' "
-    end
-    result_attrs
+    RailsVueHelpers::VueComponentBuilder.create(component_name, **props, &block)
   end
 end
